d961a35cda75b452068cee14d879a1ae9870d90d
mahestro <millanluiseduardo@gmail.com>
2017-04-13 08:53:38 +0200

    step through redux flow

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -25,10 +25,15 @@ class CoursesPage extends Component {
     this.props.dispatch(courseActions.createCourse(this.state.course));
   }
 
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input
           type="text"
@@ -43,6 +48,11 @@ class CoursesPage extends Component {
     );
   }
 }
+
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
 
 function mapStateToProps(state, ownProps) {
   return {
