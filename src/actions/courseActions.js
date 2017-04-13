b87fc0d51fbe4d4661a73d3efcd7f468dd4d104b
mahestro <millanluiseduardo@gmail.com>
2017-04-13 11:15:28 +0200

    create loac courses trunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,8 +1,19 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
+export function loadCoursesSuccess(course) {
   return {
-    type: types.CREATE_COURSE,
+    type: types.LOAD_COURSES_SUCCESS,
     course
+  };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
   };
 }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableInvariant())
+    applyMiddleware(thunk, reduxImmutableInvariant())
   );
 }
