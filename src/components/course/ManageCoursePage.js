63ae84211cab571c61e46b746b7d0a199b1847a5
mahestro <millanluiseduardo@gmail.com>
2017-04-14 17:35:14 +0200

    Use Manage Form

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,26 +2,36 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
   }
 
   render() {
     return (
-      <h1>Manage Courses</h1>
+      <CourseForm
+        allAuthors={[]}
+        course={this.state.course}
+        errors={this.state.errors} />
     );
   }
 }
 
 ManageCoursePage.propTypes = {
-
+  course: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
+  let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
   return {
-    state: state
+    course: course
   };
 }
 
