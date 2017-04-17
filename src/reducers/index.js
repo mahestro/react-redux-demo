0df8bc76cdf459b9a38f658a5c350b44483f4331
mahestro <millanluiseduardo@gmail.com>
2017-04-17 14:01:00 +0200

    Create AJAX Status Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
   courses,
-  authors
+  authors,
+  ajaxCallInProgress
 });
 
 export default rootReducer;
