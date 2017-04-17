eeea2019ef36a3aaded7f82b65455a13eb3c6709
mahestro <millanluiseduardo@gmail.com>
2017-04-17 14:58:48 +0200

    error handling

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import {beginAjaxCall} from './ajaxStatusActions';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
   return {
@@ -44,6 +44,7 @@ export function saveCourse(course) {
         dispatch(updateCourseSuccess(savedCourse)) :
         dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
+      dispatch(ajaxCallError());
       throw(error);
     });
   };
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -38,7 +38,11 @@ class ManageCoursePage extends Component {
     this.setState({saving: true});
 
     this.props.actions.saveCourse(this.state.course)
-      .then(() => this.redirect());
+      .then(() => this.redirect())
+      .catch(error => {
+        toastr.error(error);
+        this.setState({saving: false});
+      });
   }
 
   redirect() {
