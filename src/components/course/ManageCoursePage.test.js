117a2a670bed0216435126e4b35d6366fffe7aff
mahestro <millanluiseduardo@gmail.com>
2017-04-18 13:08:31 +0200

    Testing Connected React Components

---
+++ src/components/course/ManageCoursePage.test.js
@@ -0,0 +1,36 @@
+import expect from 'expect';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { ManageCoursePage } from './ManageCoursePage';
+
+function setup() {
+  const props = {
+  };
+
+  return shallow(<ManageCoursePage {...props} />);
+}
+
+describe('Manage Course Page', () => {
+  it('sets error message when trying to save empty file', () => {
+    const props = {
+      authors: [],
+      course: {
+        id: '',
+        watchHref: '',
+        title: '',
+        authorId: '',
+        length: '',
+        category: ''
+      },
+      actions: {
+        saveCourse: () => { return Promise.resolve(); }
+      }
+    };
+
+    const wrapper = mount(<ManageCoursePage {...props} />);
+    const saveButton = wrapper.find('input').last();
+    expect(saveButton.prop('type')).toBe('submit');
+    saveButton.simulate('click');
+    expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.');
+  });
+});
